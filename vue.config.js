79a79c3a8a5b6d61398d28de0cac2c5c79e62004
Joe Terry <joe@derivativegenius.com>
2024-11-27 20:38:36 -0500

    fix: Simplify build configuration
    - Remove custom webpack configuration
    - Clean up dependencies
    - Use environment variables directly

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
-const webpack = require('webpack')
 
 module.exports = defineConfig({
   transpileDependencies: [],
@@ -38,21 +37,5 @@ module.exports = defineConfig({
         }
       }
     })
-
-    // Add environment variables directly
-    config
-      .plugin('define')
-      .tap(args => {
-        Object.assign(args[0]['process.env'], {
-          VUE_APP_FIREBASE_API_KEY: JSON.stringify(process.env.VUE_APP_FIREBASE_API_KEY),
-          VUE_APP_FIREBASE_AUTH_DOMAIN: JSON.stringify(process.env.VUE_APP_FIREBASE_AUTH_DOMAIN),
-          VUE_APP_FIREBASE_PROJECT_ID: JSON.stringify(process.env.VUE_APP_FIREBASE_PROJECT_ID),
-          VUE_APP_FIREBASE_STORAGE_BUCKET: JSON.stringify(process.env.VUE_APP_FIREBASE_STORAGE_BUCKET),
-          VUE_APP_FIREBASE_MESSAGING_SENDER_ID: JSON.stringify(process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID),
-          VUE_APP_FIREBASE_APP_ID: JSON.stringify(process.env.VUE_APP_FIREBASE_APP_ID),
-          VUE_APP_API_URL: JSON.stringify(process.env.VUE_APP_API_URL)
-        })
-        return args
-      })
   }
 })
